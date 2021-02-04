bc600767254d9e3ef63a763a21b2d0a357422bed
SAMSUNG <SAMSUNG@LAPTOP-6AJ0SCTP>
2021-02-04 20:00:05 +0900

    update

--- src/example/Calendar.java
+++ src/example/Calendar.java
@@ -1,8 +1,16 @@
 package example;
+
 import java.util.Scanner;
 
 public class Calendar {
-	public static void main(String[] args) {
+
+	private final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int getmaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+
+	public void printSampleCalendar() {
 
 		System.out.println("일  월  화  수  목 금 토");
 		System.out.println("--------------------");
@@ -10,17 +18,22 @@ public class Calendar {
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-		
+		System.out.println();
+		// cal.printSampleCalendar();
+	}
+
+	public static void main(String[] args) {
+
 		// 숫자를 입력받아 입력받은 달의 최대 일수 출력하기
 		System.out.println("달을 입력하세요.");
 		Scanner scanner = new Scanner(System.in);
 		int month = scanner.nextInt();
-		
-	    int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-		
-		System.out.printf("%d월은 %d일까지 있습니다.\n",month, maxDays[month-1]);
-		
-		
+
+		Calendar cal = new Calendar();
+
+		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getmaxDaysOfMonth(month));
+		scanner.close();
+
 	}
 
 }
